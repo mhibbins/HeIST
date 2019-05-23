e99981e8eb9c53fd01f9fa1a4abcc9a97335cd6a
gibsonMatt <gibsomat@indiana.edu>
2019-05-23 14:51:18 -0400

    reformat code

--- hemiplasytool/hemiplasytool.py
+++ hemiplasytool/hemiplasytool.py
@@ -20,32 +20,43 @@ def splits_to_ms(splitTimes, taxa, reps, path_to_ms, admix=None, r=None):
     Converts inputs into a call to ms
     TODO: Add introgression
     """
-    nsamples = len(splitTimes)+1
-    call = path_to_ms + ' ' + str(nsamples) + ' ' + \
-        str(reps) + ' -T -I ' + str(nsamples)
+    nsamples = len(splitTimes) + 1
+    call = (
+        path_to_ms + " " + str(nsamples) + " " + str(reps) + " -T -I " + str(nsamples)
+    )
     for i in range(0, nsamples):
-        call += ' 1'
+        call += " 1"
     for x, split in enumerate(splitTimes):
-        call += ' -ej ' + str(split) + ' ' + str(taxa[x][0]) + ' ' +\
-            str(taxa[x][1])
+        call += " -ej " + str(split) + " " + str(taxa[x][0]) + " " + str(taxa[x][1])
 
     if admix is not None:
-        call += ' -es ' + admix[0] + ' ' + admix[1] + ' ' + '1' + ' -ej ' +\
-            admix[0] + ' ' + str(nsamples+1) + ' ' + admix[2]
+        call += (
+            " -es "
+            + admix[0]
+            + " "
+            + admix[1]
+            + " "
+            + "1"
+            + " -ej "
+            + admix[0]
+            + " "
+            + str(nsamples + 1)
+            + " "
+            + admix[2]
+        )
 
     if admix is not None:
         call += " | tail -n +4 | grep -v // > trees" + str(r) + ".tmp"
     else:
         call += " | tail -n +4 | grep -v // > trees.tmp"
-    return(call)
+    return call
 
 
 def seq_gen_call(treefile, path, s=0.05):
     """
     Make seq-gen call.
     """
-    return(path + ' -m HKY -l 1 -s ' + str(s) + ' -wa <"' +
-                                                treefile + '" > seqs.tmp')
+    return path + " -m HKY -l 1 -s " + str(s) + ' -wa <"' + treefile + '" > seqs.tmp'
 
 
 def call_programs(ms_call, seqgencall, treefile, ntaxa):
@@ -84,19 +95,29 @@ def summarize(results):
     for key, val in results.items():
         c_disc_follow += val[0]
         c_conc_follow += val[1]
-    return([c_disc_follow, c_conc_follow])
+    return [c_disc_follow, c_conc_follow]
 
 
-def write_output(summary, mutation_counts_c, mutation_counts_d, reduced, counts, speciesTree, admix, traits, filename):
-    out1 = open(filename, 'w')
+def write_output(
+    summary,
+    mutation_counts_c,
+    mutation_counts_d,
+    reduced,
+    counts,
+    speciesTree,
+    admix,
+    traits,
+    filename,
+):
+    out1 = open(filename, "w")
 
     # CALCULATE SUMMARY STATS
     derived = []
     tree = speciesTree
     for key, val in traits.items():
-        if val == '1':
+        if val == "1":
             derived.append(key)
-            tree = tree.replace(key, (key+"*"))
+            tree = tree.replace(key, (key + "*"))
 
     mix_range = list(range(2, len(derived)))
     true_hemi = 0
@@ -116,66 +137,115 @@ def write_output(summary, mutation_counts_c, mutation_counts_d, reduced, counts,
     sum_from_introgression = 0
     sum_from_species = 0
     for i, topology_count in enumerate(counts):
-        if i != len(counts)-1:
+        if i != len(counts) - 1:
             sum_from_introgression += topology_count
         else:
             sum_from_species = topology_count
 
     # INPUT SUMMARY
     out1.write("### INPUT SUMMARY ###\n\n")
-    out1.write("The species tree is " + speciesTree + '\n\n')
-    t = tree.replace(";", '')
+    out1.write("The species tree is " + speciesTree + "\n\n")
+    t = tree.replace(";", "")
     t = Phylo.read(io.StringIO(t), "newick")
     Phylo.draw_ascii(t, out1, column_width=40)
 
-
     # INPUT SUMMARY
-    out1.write(str(len(derived)) + " taxa have the derived state: " + ', '.join(derived) + '\n\n')
+    out1.write(
+        str(len(derived))
+        + " taxa have the derived state: "
+        + ", ".join(derived)
+        + "\n\n"
+    )
     for event in admix:
-        out1.write("Introgression from taxon " + event[1] + " into taxon " + event[2] + " occurs at time " + event[0] + " with probability " + event[3] + '\n')
-    out1.write('\n')
+        out1.write(
+            "Introgression from taxon "
+            + event[1]
+            + " into taxon "
+            + event[2]
+            + " occurs at time "
+            + event[0]
+            + " with probability "
+            + event[3]
+            + "\n"
+        )
+    out1.write("\n")
 
     # OUTPUT SUMMARY
     out1.write("\n### OUTPUT SUMMARY ###\n\n")
-    out1.write('"True" hemiplasy (1 mutation) occurs ' + str(true_hemi) + " time(s)\n\n")
-    out1.write('Combinations of hemiplasy and homoplasy (1 < # mutations < 3) occur ' + str(mix) + " time(s)\n\n")
-    out1.write('"True" homoplasy (>= 3 mutations) occurs ' + str(true_homo) + " time(s)\n\n")
+    out1.write(
+        '"True" hemiplasy (1 mutation) occurs ' + str(true_hemi) + " time(s)\n\n"
+    )
+    out1.write(
+        "Combinations of hemiplasy and homoplasy (1 < # mutations < 3) occur "
+        + str(mix)
+        + " time(s)\n\n"
+    )
+    out1.write(
+        '"True" homoplasy (>= 3 mutations) occurs ' + str(true_homo) + " time(s)\n\n"
+    )
     out1.write(str(summary[0]) + " loci have a discordant gene tree\n")
-    out1.write(str(summary[1]-summary[0]) + " loci are concordant with the species tree\n\n")
-    out1.write(str(sum_from_introgression) + " loci originate from an introgressed history\n")
+    out1.write(
+        str(summary[1] - summary[0]) + " loci are concordant with the species tree\n\n"
+    )
+    out1.write(
+        str(sum_from_introgression) + " loci originate from an introgressed history\n"
+    )
     out1.write(str(sum_from_species) + " loci originate from the species history\n\n")
     if reduced is not None:
         out1.write("In cases with combinations of hemiplasy and homoplasy:\n\n")
         for key, val in reduced.items():
             val = [str(v) for v in val]
-            out1.write("Taxon " + key + " mutated to the derived state " + val[0] + " time(s), and inherited it from an ancestral population " + val[1] + " time(s)\n")
+            out1.write(
+                "Taxon "
+                + key
+                + " mutated to the derived state "
+                + val[0]
+                + " time(s), and inherited it from an ancestral population "
+                + val[1]
+                + " time(s)\n"
+            )
 
     # DETAILED OUTPUT
     out1.write("\n\n### DETAILED OUTPUT ###\n\n")
     out1.write("On concordant trees:\n")
     out1.write("# Mutations\t# Trees\n")
     for item in mutation_counts_c:
-        out1.write(str(item[0]) + '\t\t' + str(item[1]) + '\n')
+        out1.write(str(item[0]) + "\t\t" + str(item[1]) + "\n")
     out1.write("\nOn discordant trees:\n")
     out1.write("# Mutations\t# Trees\n")
     for item in mutation_counts_d:
-        out1.write(str(item[0]) + '\t\t' + str(item[1]) + '\n')
+        out1.write(str(item[0]) + "\t\t" + str(item[1]) + "\n")
 
     if reduced is not None:
-        out1.write('\nDerived mutation inheritance patterns for trees with fewer mutations than derived taxa:\n')
-        out1.write('\tTerm\tInherited from anc node\n')
+        out1.write(
+            "\nDerived mutation inheritance patterns for trees with fewer mutations than derived taxa:\n"
+        )
+        out1.write("\tTerm\tInherited from anc node\n")
         for key, val in reduced.items():
             val = [str(v) for v in val]
-            out1.write('Taxa ' + key + '\t' + '\t'.join(val) + '\n')
+            out1.write("Taxa " + key + "\t" + "\t".join(val) + "\n")
 
     out1.write("\nOf the replicates that follow species site pattern:\n")
-    out1.write(str(summary[0]) + " were discordant\n" + str(summary[1]-summary[0]) + " were concordant\n\n")
+    out1.write(
+        str(summary[0])
+        + " were discordant\n"
+        + str(summary[1] - summary[0])
+        + " were concordant\n\n"
+    )
 
     for i, topology_count in enumerate(counts):
-        if i != len(counts)-1:
-            out1.write(str(topology_count) + " replicates matching the species pattern were from introgression tree " + str(i+1) + "\n")
+        if i != len(counts) - 1:
+            out1.write(
+                str(topology_count)
+                + " replicates matching the species pattern were from introgression tree "
+                + str(i + 1)
+                + "\n"
+            )
         else:
-            out1.write(str(topology_count) + " replicates matching the species pattern were from the species tree\n")
+            out1.write(
+                str(topology_count)
+                + " replicates matching the species pattern were from the species tree\n"
+            )
 
     out1.close()
 
@@ -190,7 +260,7 @@ def plot_mutations(results_c, results_d):
     disc_dic = {}
     objs = objs_c + objs_d
     objs = set(objs)
-    x = np.array(list(range(1, max(objs)+1)))
+    x = np.array(list(range(1, max(objs) + 1)))
     y1 = []
     y2 = []
     width = 0.2
@@ -213,18 +283,18 @@ def plot_mutations(results_c, results_d):
     for o in x:
         labels.append(str(o))
     _, ax = plt.subplots()
-    p1 = ax.bar(x, y1, width, color='#484041')
-    p2 = ax.bar(x+width, y2, width, color='#70ee9c')
+    p1 = ax.bar(x, y1, width, color="#484041")
+    p2 = ax.bar(x + width, y2, width, color="#70ee9c")
     ax.set_xticks(x + width / 2)
     ax.set_xticklabels(labels)
-    plt.ylabel('Count')
-    plt.xlabel('# Mutations')
-    ax.legend((p1[0], p2[0]), ('Concordant trees', 'Discordant trees'))
-    plt.savefig('mutation_dist.png', dpi=250)
+    plt.ylabel("Count")
+    plt.xlabel("# Mutations")
+    ax.legend((p1[0], p2[0]), ("Concordant trees", "Discordant trees"))
+    plt.savefig("mutation_dist.png", dpi=250)
 
 
 def readInput(file):
-    f = open(file, 'r')
+    f = open(file, "r")
     splits = []
     taxa = []
     traits = {}
@@ -253,7 +323,7 @@ def readInput(file):
             l = line.replace("\n", "").split()
             admix.append(l)
 
-    return(splits, taxa, traits, tree, admix)
+    return (splits, taxa, traits, tree, admix)
 
 
 def summarize_inherited(inherited):
@@ -270,26 +340,26 @@ def summarize_inherited(inherited):
             elif event[1] == 0:
                 reduced[event[0]][1] += 1
 
-    #print('\nDerived mutation inheritance patterns for trees with fewer\
+    # print('\nDerived mutation inheritance patterns for trees with fewer\
     #     mutations than derived taxa:\n')
-    #print('\tTerm\tInherited from anc node')
-    #for key, val in reduced.items():
+    # print('\tTerm\tInherited from anc node')
+    # for key, val in reduced.items():
     #    val = [str(v) for v in val]
     #    print('Taxa ' + key + '\t' + '\t'.join(val))
-    return(reduced)
+    return reduced
 
 
 def update_count(tree, dic):
     for key, val in dic.items():
         if seqtools.compareToSpecies(key, tree):
             dic[key] += 1
-    return(dic)
+    return dic
 
 
 def write_unique_trees(focal_trees, filename):
     unique = []
     counts = {}
-    out1 = open(filename, 'a')
+    out1 = open(filename, "a")
 
     for i, tree in enumerate(focal_trees):
         if i == 0:
@@ -306,7 +376,7 @@ def write_unique_trees(focal_trees, filename):
                 counts[tree] = 1
     out1.write("\n### OBSERVED GENE TREES ###\n\n")
     for tree in unique:
-        t = tree.replace(";", '')
+        t = tree.replace(";", "")
         t = Phylo.read(io.StringIO(tree), "newick")
         Phylo.draw_ascii(t, out1, column_width=40)
         for key, val in counts.items():
